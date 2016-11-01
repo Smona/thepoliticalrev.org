94a213c1a9e1d06d3921913e8436b9f102457a04
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-01 16:40:10 -0500

    Candidate images!!
    I pulled candidates from ourrevolution.com. Some are missing, but we
    can add those manually

--- assets/js/react/Candidate.js
+++ assets/js/react/Candidate.js
@@ -1,13 +1,18 @@
 import React from 'react';
 
 class Candidate extends React.Component {
+	encodeName(name) {
+		name = name.replace(/ /g, '-').toLowerCase();
+		return name;
+	}
 	render() {
 		var me = this.props.me;
 		var name = me["Candidate Name"];
 		return (
-			<a href={me.Link_link}><div className="candidate">
-				<h3>{name}</h3>
-				<p>{me.State.split(' ')[0]}</p>
+			<a href={me.Link_link}><div className="candidate" style={{backgroundImage: 'url(https://ourrevolution.com/img/candidates/profile/' + this.encodeName(name) + '.jpg)'}}>
+				<header>
+					<h3>{name}</h3> <span>{me.State.split(' ')[0]}</span>			
+				</header>
 			</div></a>
 		);
 	}
