2d0daf986c99a747ad3e227742d70ac7445e1c8b
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-01 17:08:55 -0500

    Found the remaining candidate images

--- assets/js/react/Candidate.js
+++ assets/js/react/Candidate.js
@@ -1,17 +1,17 @@
 import React from 'react';
 
 class Candidate extends React.Component {
-	encodeName(name) {
-		name = name.replace(/ /g, '-').toLowerCase();
-		return name;
+	getPicture(candidateLink) {
+		let list = candidateLink.split('/');
+		return 'url(https://ourrevolution.com/img/candidates/profile/' + list[list.length - 1] + '.jpg)';
 	}
 	render() {
 		var me = this.props.me;
 		var name = me["Candidate Name"];
 		return (
-			<a href={me.Link_link}><div className="candidate" style={{backgroundImage: 'url(https://ourrevolution.com/img/candidates/profile/' + this.encodeName(name) + '.jpg)'}}>
+			<a href={me.Link_link}><div className="candidate" style={{backgroundImage: this.getPicture(me.Link_link)}}>
 				<header>
-					<h3>{name}</h3> <span>{me.State.split(' ')[0]}</span>			
+					<h3>{name}</h3> <span>{me.State}</span>
 				</header>
 			</div></a>
 		);
