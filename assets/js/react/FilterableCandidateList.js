dc34c76fccc35452a113b1f3035f0e47ebb4276b
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-02 01:12:46 -0500

    Further JS shrinking (enable react production mode)

--- assets/js/react/FilterableCandidateList.js
+++ assets/js/react/FilterableCandidateList.js
@@ -3,20 +3,19 @@ import ReactDom from 'react-dom';
 import CandidateList from './CandidateList';
 import PapaParse from 'papaparse';
 import $ from 'jquery';
+
 require('./candidate-list.sass');
 
-// var candidates = ['Bernie', 'Zephyr', 'Russ'];
-var spreadsheetID = '1rIiEAMjfPTa5j4ttsehoFi939I9FfnqyE5dKy802474';
-var sheetID = '163578027';
+// var spreadsheetID = '1rIiEAMjfPTa5j4ttsehoFi939I9FfnqyE5dKy802474';
+// var sheetID = '163578027';
 var candidates = {};
 $.get({
 	url: '/data/Candidates2016.csv',
-	success: function (data, status) {
+	success: function (data) {
 		PapaParse.parse(data, {
 			header: true,
-			complete: function (results, file) {
+			complete: function (results) {
 				candidates = results;
-				console.log(candidates);
 				ReactDom.render(
 					<FilterableCandidateList items={candidates} />,
 					document.getElementById('root')
@@ -26,15 +25,15 @@ $.get({
 	}
 });
 
-class FilterableCandidateList extends React.Component {
+export default class FilterableCandidateList extends React.Component {
 	render() {
 		return (
 			<div className="FilterableCandidateList">
-			{/*<label>Filter by state:</label>
+			{/* <label>Filter by state:</label>
 				<select>
 					<option>Alabama</option>
 					<option>Alaska</option>
-				</select>*/}
+				</select> */}
 				<CandidateList items={this.props.items} />
 			</div>
 		);
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,29 @@
-var dir = __dirname + '/assets/js'
-var webpack = require("webpack");
+const path = require('path');
+const webpack = require('webpack');
+
+var dir = path.join(__dirname, '/assets/js');
 
 module.exports = {
-    entry: dir + "/react/FilterableCandidateList.js",
-    output: {
-        path: dir,
-        filename: "bundle.js"
-    },
-    module: {
-			loaders: [
-    		{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
-        { test: /\.sass$/, loader: "style!css?autoprefixer?minifySelectors!sass"}
-  		]
-    },
-    watch: true,
-    plugins: [new webpack.optimize.UglifyJsPlugin()]
+	entry: dir + '/react/FilterableCandidateList.js',
+	output: {
+		path: dir,
+		filename: 'bundle.js'
+	},
+	module: {
+		loaders: [
+			{test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader'},
+			{test: /\.sass$/, loader: 'style!css?autoprefixer?minifySelectors!sass'}
+		]
+	},
+	watch: true,
+	plugins: [
+		new webpack.DefinePlugin({
+			'process.env': {
+				NODE_ENV: JSON.stringify('production')
+			}
+		}),
+		new webpack.optimize.UglifyJsPlugin(),
+		new webpack.optimize.OccurrenceOrderPlugin(),
+		new webpack.optimize.DedupePlugin()
+	]
 };
