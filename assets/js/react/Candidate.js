01bb75ea79b639023b59e02ce269fb6c05bcc6d2
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-02 22:01:43 -0500

    Add candidate info link

--- assets/js/react/Candidate.js
+++ assets/js/react/Candidate.js
@@ -17,22 +17,25 @@ class Candidate extends React.Component {
 			expanded: !prevState.expanded
 		}));
 	}
+	followLink(e) {
+		e.stopPropagation();
+	}
 	render() {
 		var me = this.props.me;
 		var name = me['Candidate Name'];
 
 		return (
 			<a href="#" onClick={this.toggleExpand.bind(this)}><div className="candidate" style={{backgroundImage: this.getPicture(me.Link_link)}}>
-						<header style={{opacity: (this.state.expanded ? '0' : '1')}}>
- 							<h3>{name}</h3> <span>{me.State}</span>
- 						</header>
-					)}
+					<header style={{opacity: (this.state.expanded ? '0' : '1')}}>
+						<h3>{name}</h3> <span>{me.State}</span>
+					</header>
 
 					<section style={{opacity: (this.state.expanded ? '1' : '0')}}>
 						<h3>{name}</h3>
 						<span>{me.State}</span>
 						{me.Region ? (<span>{me.Region}</span>) : null}
 						<span>{me.Position}</span>
+						<a href={me.Link_link} onClick={this.followLink.bind(this)}><i className="fa fa-info-circle button-icon" style={{fontSize: '3em'}}></i></a>
 					</section>
 			</div></a>
 		);
