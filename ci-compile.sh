d80b9a5666ecea46bfc821fcffd47f261fa54510
Mason Bourgeois <mason.bourgeois@gmail.com>
2016-11-09 20:52:36 -0600

    Include _config.yaml

--- ci-compile.sh
+++ ci-compile.sh
@@ -1,4 +1,4 @@
 npm install
 webpack -p --config ./webpack.production.config.js
 gem install jekyll jekyll-sitemap jekyll-seo-tag jekyll-feed
-jekyll build -d public_html --config _github-config.yml _github-config.yml
+jekyll build -d public_html --config _config.yml _github-config.yml
